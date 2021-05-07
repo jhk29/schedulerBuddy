87624168397cbfda581231b2d8e12ccf5add782f
Sam Kim <jhk29@sfu.ca>
2021-05-07 13:22:54 -0700

    4: APIs for calendar events

---
+++ server/models/Event.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const EventSchema = new Schema({
+  userId: {
+    type: Schema.Types.ObjectId,
+    ref: "users",
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: false,
+  },
+  start: {
+    type: Date,
+    required: true,
+  },
+  end: {
+    type: Date,
+  },
+  allDay: {
+    type: Boolean,
+    default: false,
+  },
+  createdOn: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = Event = mongoose.model("event", EventSchema);
