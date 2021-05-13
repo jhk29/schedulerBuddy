dc96fe8cf7f48db23bd6ad3fbf14372bf9b39910
Sam Kim <jhk29@sfu.ca>
2021-05-12 19:11:39 -0700

    test heroku

--- server/server.js
+++ server/server.js
@@ -30,11 +30,11 @@ app.use("/api/users", users);
 app.use("/api/todo", todo);
 app.use("/api/event", event);
 
-if(process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, 'client', 'build')));
+if(process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
 
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
+    res.sendFile(path.resolve(__dirname, '../client', 'build', 'index.html'))
   });
 
 }
