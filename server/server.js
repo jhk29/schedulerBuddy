cf9fe6ec9c0ffffe6089bdb26135829fab0de120
Sam Kim <jhk29@sfu.ca>
2021-05-12 19:04:05 -0700

    heroku deployment

--- server/server.js
+++ server/server.js
@@ -30,6 +30,15 @@ app.use("/api/users", users);
 app.use("/api/todo", todo);
 app.use("/api/event", event);
 
+if(process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'client', 'build')));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
+  });
+
+}
+
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server is up and runing on port ${port}!`));
